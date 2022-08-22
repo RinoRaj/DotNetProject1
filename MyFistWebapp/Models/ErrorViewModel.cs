5ea78c218851cd1ec6de66d312f78d4e2caebe94
Rino <rrino@wagepoint.com>
2022-08-22 15:45:20 +0530

    Second commit

---
+++ MyFistWebapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyFistWebapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
